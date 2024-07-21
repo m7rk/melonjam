88340840ad107d2b00968d9ef977125a04cd6686
DremaDrag0n <142751293+NoodleDood1e@users.noreply.github.com>
2024-07-22 00:16:52 +0500

    Enemies now emit particles, and have colour coded eyes

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -18,6 +18,18 @@ public class Minion : MonoBehaviour
     public SpriteRenderer[] srOutline;
     public SpriteRenderer[] srColor;
 
+    public ParticleSystem upParticle;
+    public ParticleSystem downParticle;
+    public ParticleSystem leftParticle;
+    public ParticleSystem rightParticle;
+
+    public Color colourUp;
+    public Color colourDown;
+    public Color colourLeft;
+    public Color colourRight;
+
+    public SpriteRenderer[] srEyes;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +38,11 @@ public class Minion : MonoBehaviour
 
     public void setSpriteOrder(int idx)
     {
-        srOutline[0].sortingOrder = idx;
-        srOutline[1].sortingOrder = idx;
+        srOutline[0].sortingOrder = idx + 1;
+        srOutline[1].sortingOrder = idx + 1;
+
+        srEyes[0].sortingOrder = idx;
+        srEyes[1].sortingOrder = idx;
 
         srColor[0].sortingOrder = idx - 1;
         srColor[1].sortingOrder = idx - 1;
@@ -39,6 +54,27 @@ public class Minion : MonoBehaviour
         Invoke("delete", 0.2f);
         move.SetActive(false);
         dead.SetActive(true);
+
+
+
+
+        if (direction == "up")
+        {
+            upParticle.Play();
+        }
+        else if (direction == "down")
+        {
+            downParticle.Play();
+        }
+        else if (direction == "left")
+        {
+            leftParticle.Play();
+        }
+        else if (direction == "right")
+        {
+            rightParticle.Play();
+        }
+
     }
 
     public void delete()
@@ -51,18 +87,30 @@ public class Minion : MonoBehaviour
         direction = d;
         if(d == "up")
         {
+            srEyes[0].color = colourUp;
+            srEyes[1].color = colourUp;
+
             upArrow.SetActive(true);
         }
         else if(d == "down")
         {
+            srEyes[0].color = colourDown;
+            srEyes[1].color = colourDown;
+
             downArrow.SetActive(true);
         }
         else if(d == "left")
         {
+            srEyes[0].color = colourLeft;
+            srEyes[1].color = colourLeft;
+
             leftArrow.SetActive(true);
         }
         else if(d == "right")
         {
+            srEyes[0].color = colourRight;
+            srEyes[1].color = colourRight;
+
             rightArrow.SetActive(true);
         }
 
